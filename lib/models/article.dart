f001efa9a9f3a8b79987a8a9e1383e10e9538b77
danield76799 <daanentamara+1@gmail.com>
2026-05-09 09:43:52 +0200

    Fix Article model - proper Dart syntax

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -1,23 +1,23 @@
 class Article {
-  final string id;
-  final string title;
-  final string description;
-  final string link;
+  final String id;
+  final String title;
+  final String description;
+  final String link;
   final DateTime pubDate;
-  final string? thumbnailUrl;
-  final string source;
-  final string? category;
+  final String? thumbnailUrl;
+  final String source;
+  final String? category;
 
   Article({
     required this.id,
     required this.title,
     required this.description,
     required this.link,
-    requirred this.pubDate,
+    required this.pubDate,
     this.thumbnailUrl,
     required this.source,
     this.category,
   });
 
-  bool get isNew = DateTime.now.difference(pubDate).inMinutes < 60;
+  bool get isNew => DateTime.now().difference(pubDate).inMinutes < 60;
 }
