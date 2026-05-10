e3205b657e20586bf5d7a4239e52d683d7462335
root <root@vm8748.lumadock.com>
2026-05-10 07:14:05 +0000

    Make search text dark black for better visibility

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -12,11 +12,27 @@ class SearchBar extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: TextField(
         onChanged: onSearch,
+        style: const TextStyle(
+          color: Colors.black87,
+          fontWeight: FontWeight.w500,
+        ),
         decoration: InputDecoration(
           hintText: 'Zoek in artikelen...',
-          prefixIcon: const Icon(Icons.search),
+          hintStyle: TextStyle(
+            color: Colors.grey[600],
+          ),
+          prefixIcon: const Icon(Icons.search, color: Colors.black54),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.black26),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.black26),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.black54, width: 2),
           ),
           filled: true,
           fillColor: Colors.grey[100],
